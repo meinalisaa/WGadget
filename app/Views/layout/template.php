ed8633aca523e1057541b7cafcdf6ec6bef66dda
dindajuliap <dindajuliap30@gmail.com>
2022-05-31 19:09:35 +0700

    logo

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -9,7 +9,10 @@
 
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand"><b>WGadget</b></a>
+      <a class="navbar-brand">
+        <img src="<?= base_url('/images/logo/logo-ungu.png') ?>" style="max-height: 30px">
+        <b>WGadget</b>
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
