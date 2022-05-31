b1c3fbd83a8765f058e05712abbb66a0f8085bc9
dindajuliap <dindajuliap30@gmail.com>
2022-05-31 18:22:54 +0700

    beranda frontend

--- app/Controllers/Beranda.php
+++ app/Controllers/Beranda.php
@@ -1,21 +1,33 @@
 <?php
   namespace App\Controllers;
 
-  class Beranda extends BaseController{
+  use CodeIgniter\RESTful\ResourceController;
+  use CodeIgniter\API\ResponseTrait;
+  use App\Models\HpModel;
+
+  class Beranda extends ResourceController{
+    use ResponseTrait;
+
     public function index(){
       $data['judul'] = 'WGadget';
-      $url = base_url('/getHp');
+      $url = base_url('/beranda/getHp');
 
-      $curl = service('curlrequest');
-      $response = $curl->request('GET', $url, [
-  			"headers" => [
-  				"Accept" => "application/json"
-  			]
-  		]);
+       $curl = service('curlrequest');
+       $response = $curl->request('GET', $url, [
+  		 	"headers" => [
+  		 		"Accept" => "application/json"
+  		 	]
+  		 ]);
 
-      if($response->getStatusCode() == 200){
-        $data['database'] = json_decode($response->getBody());
+       if($response->getStatusCode() == 200){
+         $data['database'] = json_decode($response->getBody());
         echo view('pages\beranda', $data);
       }
+    }
+
+    public function getHp(){
+      $model      = new HpModel();
+      $data['db'] = $model->getAll();
+      return $this->respond($data['db'], 200);
     }
   }
