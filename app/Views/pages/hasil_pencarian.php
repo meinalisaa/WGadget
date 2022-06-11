1da7fb5971952c80da946a5d5816273c1f701da2
meinalisaa <meinalisa02@gmail.com>
2022-06-11 16:28:54 +0700

    nambah href

--- app/Views/pages/hasil_pencarian.php
+++ app/Views/pages/hasil_pencarian.php
@@ -6,10 +6,12 @@
     <div class="row">
       <?php if($database){ foreach($database as $db){ ?>
         <div class="col-md-2" style="margin-top: 10px; text-align: center">
-          <div class="card" style="padding: 20px 10px 70px 10px; width: 100%; height: 200px">
-            <img src="<?= base_url('/assets/img/hp/'.$db->foto_hp) ?>" style="height: 80%; margin-left: auto; margin-right: auto">
-            <p style="margin-top: 10px"><?= $db->nama_hp ?></p>
-          </div>
+          <a href="<?= base_url('beranda/hp/'.$db->id_hp) ?>">
+            <div class="card" style="padding: 20px 10px 70px 10px; width: 100%; height: 200px">
+              <img src="<?= base_url('/assets/img/hp/'.$db->foto_hp) ?>" style="height: 80%; margin-left: auto; margin-right: auto">
+              <p style="margin-top: 10px; color: black;"><?= $db->nama_hp ?></p>
+            </div>
+          </a>
         </div>
       <?php }} else{ ?>
         <div class="alert alert-danger alert-dismissible fade show alert-dismissible fade show col-md-12 mt-3" role="alert">
@@ -18,4 +20,10 @@
       <?php } ?>
     </div>
   </div>
+
+  <style type="text/css">
+    .card:hover {
+      background: #F0F0F0;
+    }
+  </style>
 <?= $this->endSection() ?>
